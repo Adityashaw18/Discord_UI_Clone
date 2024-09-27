4dce60fd744b695715ac2fba234c062ad438060d
Adityashaw18 <adityashaw59@outlook.com>
2024-09-27 14:35:58 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Whitney:["Whitney"],
+        Ginto:["Ginto"],
+        GintoNord:["GintoNord"],
+        ggSans:["ggSans"],
+      },
+      colors: {
+        violetblue: "#404EED",
+        lightwhite: "#fff",
+      }
+    },
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
